63f5cd4d29e7c6efe875204a01ccbac1b2d06e8d
thaizerbinatti <thatazerb@gmail.com>
2023-05-09 21:42:07 -0300

    Atividade 03 - JS

---
+++ JHONATTAN/WORKSPACE/JS/03_Strings/main2.js
@@ -0,0 +1,13 @@
+var fruta1 = "Morango"
+var fruta2 = "Uva"
+var fruta3 = "Pera"
+var fruta4 = "Banana"
+var fruta5 = "         Jabuticaba"
+const novaMensagem = fruta2.replace('Uva', 'SãoPaulo')
+console.log (fruta2.length)
+console.log (fruta1.substring(0,3))
+console.log (fruta2.substring(0,3))
+console.log (fruta3.substring(0,3))
+console.log (fruta5.trim())
+console.log (novaMensagem.toLowerCase())
+console.log (fruta1 + " " + fruta1 + " " + fruta2 + " " + fruta3 + fruta4 + fruta5 )
