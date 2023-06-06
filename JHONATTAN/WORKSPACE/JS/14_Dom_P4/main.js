f12a5759cf8515f488314ac3302ad409672094fa
jhonattanVenturini <jhonattan.venturini@outlook.com>
2023-06-06 15:41:43 -0300

    Atividade 14 - JS

---
+++ JHONATTAN/WORKSPACE/JS/14_Dom_P4/main.js
@@ -0,0 +1,21 @@
+const corpo = document.querySelector('body')
+
+var botão = document.querySelector('button')
+botão.style.background = "red"
+botão.style.color = "white"
+
+var botão2 = document.getElementById('botão2')
+botão2.style.background = 'green'
+botão2.style.color = 'white'
+
+var label = document.querySelector("label")
+label.style.background = 'red'
+label.style.color = "white"
+label.style.border = '1px solid black'
+label.style.textAlign = 'center'
+
+function BotãoIncluir() {
+    let nome = document.querySelector('nome')
+    let dataNasc = document.querySelector('dataNasc')
+    let email = document.querySelector('email')
+}
