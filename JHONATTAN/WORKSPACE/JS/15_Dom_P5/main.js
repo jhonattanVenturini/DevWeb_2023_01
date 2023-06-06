ea53fa0aae699442e69114c8fae7673778832153
jhonattanVenturini <jhonattan.venturini@outlook.com>
2023-06-05 21:15:33 -0300

    Atividade 15 - js

---
+++ JHONATTAN/WORKSPACE/JS/15_Dom_P5/main.js
@@ -0,0 +1,15 @@
+
+let Login = document.getElementById('Login')
+Login.style.background = 'black'
+Login.style.color = 'white'
+
+function Login() {
+    let Usuario = document.getElementById('usuario')
+    let passaword = document.getElementById('passaword')
+}
+
+
+
+
+
+
