1b333454ad4a25e13b0904ddd6da4b79b7faccde
jhonattanVenturini <jhonattan.venturini@outlook.com>
2023-06-12 21:08:03 -0300

    Atividade 14 - JS

--- JHONATTAN/WORKSPACE/JS/14_Dom_P4/main.js
+++ JHONATTAN/WORKSPACE/JS/14_Dom_P4/main.js
@@ -1,20 +1,48 @@
-let nome = document.getElementById('nome').value;
-let dataNasc = document.getElementById('dataNasc').value;
-let email = document.getElementById('email').value;
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
 
 
-let inscritos = [];
-function validarEmail(email) {
-    const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-    return regex.test(email);
-}
-function CalcularIdade(dataNasc) {
-    const hoje = new Date()
-    const dataNascimento = new Date(dataNasc)
-    let idade = hoje.getFullYear() - dataNascimento.getFullYear()
-    const mes = hoje.getMonth() - dataNascimento.getMonth()
-    if (mes < 0 || mes === 0 && hoje.getDate() < dataNascimento.getDate()) {
-        idade--;
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const horario = document.querySelector('#horario');
+const msg = document.querySelector('.msg');
+const msg_email = document.querySelector('.msg_email');
+const userList = document.querySelector('#users');
+
+
+function onSubmit(e) {
+    e.preventDefault();
+    
+    if (nameInput.value === '' || emailInput.value === '') {
+     
+        msg.classList.add('error');
+        msg.innerHTML = 'Por favor, preencha os dados.';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+  
+        const li = document.createElement('li');
+        li.appendChild(
+            document.createTextNode(
+                `${nameInput.value} : ${emailInput.value} : ${horario.value}`
+            )
+        );
+        userList.appendChild(li);
+       
+        nameInput.value = '';
+        emailInput.value = '';
+        horario.getElementsByTagName('option')[0].selected = 'selected';
+        nameInput.focus(); 
     }
-    return idade;
 }
+
+emailInput.addEventListener('change', (e) => {
+    let padrao = new RegExp(/.*@.*\..*/i);
+    if (!padrao.test(emailInput.value)) {
+        
+        msg_email.classList.add('error');
+        msg_email.innerHTML = 'Por favor, insira um e-mail válido.';
+        setTimeout(() => msg.remove(), 3000);
+    }
+});
